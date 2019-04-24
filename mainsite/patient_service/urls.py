54a7a80124194dd073a92fda63af36a08bb39270
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-04-24 15:21:34 -0400

    playing around with service paths

--- mainsite/patient_service/urls.py
+++ mainsite/patient_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('', views.opt, name = 'index')
+    path('', views.opt, name = 'index'),
+    path('test/', views.test, name='test'),
 ]
