59790d4e9d013ae982f02ecdbd3ac092358f0134
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-04-29 20:27:08 -0400

    patient data model and medicalclinic database connection

--- mainsite/patient_service/models.py
+++ mainsite/patient_service/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class patient(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField()
+    phonenumber = models.CharField(max_length=20)
+    address = models.TextField()
