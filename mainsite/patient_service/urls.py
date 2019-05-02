123bc9050e85d7cd4c0fd21f58b309fe64c2a5d0
Rahul Lodha <rahul@152.sub-97-40-145.myvzw.com>
2019-05-02 10:36:34 -0400

    new end-points of get-patient by id and get-patient by email added

--- mainsite/patient_service/urls.py
+++ mainsite/patient_service/urls.py
@@ -6,4 +6,5 @@ urlpatterns= [
     path('', views.opt, name = 'index'),
     path('test/', views.test, name='test'),
     path('get-patient/<str:patient_id>/', views.get_patient, name = 'get-patient-by-patient-id'),
+    path('get-patient-email/<str:patient_email>/', views.get_patient_by_email, name = 'get-patient-by-patient-email'),
 ]
--- mainsite/patient_service/views.py
+++ mainsite/patient_service/views.py
@@ -13,7 +13,10 @@ def test(request):
 
 def get_patient(request, patient_id):
     patients =patient.objects.filter(id = patient_id)
-    data = serializers.serialize('json', patients, fields= ('email', 'firstname', 'lastname',
-                                                           'phonenumber', 'address'))
-    data = json.loads(data)
+    data = serializers.serialize('json', patients)
+    return HttpResponse(data, content_type = 'application/json')
+
+def get_patient_by_email(request, patient_email):
+    patients =patient.objects.filter(email = patient_email)
+    data = serializers.serialize('json', patients)
     return HttpResponse(data, content_type = 'application/json')
