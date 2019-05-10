935daa01ec6bbee6b787c51ca6b3de164a62874f
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-05-10 13:28:31 -0400

    initial setup script updated with virtual env setup and db migration

--- setup.sh
+++ setup.sh
@@ -2,3 +2,18 @@
 
 echo "creating containers"
 docker-compose up -d
+
+if [ ! -d "$DIRECTORY" ]; then
+  echo creating virtual environment
+  mkdir .venv
+  python3 -m venv .venv
+fi
+echo "activating virtual environment"
+source .venv/bin/activate
+
+echo "installing dependencies"
+pip install -r requirements.txt
+
+echo "Create DB schema"
+python mainsite/manage.py makemigrations
+python mainsite/manage.py migrate
