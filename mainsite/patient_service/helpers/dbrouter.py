79c86057502477d80d7612366fdc377a417f1935
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-05-22 20:30:50 -0400

    db router for dynamically selecting database at runtime

---
+++ mainsite/patient_service/helpers/dbrouter.py
@@ -0,0 +1,25 @@
+from .. import env
+
+
+class DBRouter:
+
+    def db_for_read(self, model, **hints):
+        return self.select_db()
+
+    def db_for_write(self, model, **hints):
+        return self.select_db()
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'test' or obj2._meta.app_label == 'test':
+            return None
+        return None
+
+    # def allow_migrate(self, db, app_label, model_name=None, **hints):
+    #     if env.env_list.get('ENV') == 'dockerized':
+    #         return db == 'dockerized'
+    #     return db == 'local'
+
+    def select_db(self):
+        if env.env_list.get('ENV') is not None:
+            return env.env_list.get('ENV')
+        return 'local'
